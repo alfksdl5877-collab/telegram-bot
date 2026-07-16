30923cd7644edb788bbefe040537357945145b5f
alfksdl5877-collab <alfksdl5877@gmail.com>
2026-07-16 22:16:02 +0900

    Update config.py

--- config.py
+++ config.py
@@ -16,18 +16,17 @@
 import os
 
 # 필수 및 설정 변수 정의
+import os
+
+# Render 환경 변수에서 값 가져오기
 API_ID = int(os.environ.get('API_ID', 0))
 API_HASH = os.environ.get('API_HASH', "")
 BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
 DATABASE_URI = os.environ.get('DATABASE_URI', "")
 DATABASE_NAME = os.environ.get('DATABASE_NAME', "BetterAutoFilterBot")
-
-# main.py에서 호출하는 변수들 추가
+AUTH_USERS = [int(x) for x in os.environ.get('AUTH_USERS', "0").split()]
+AUTH_GROUPS = [int(x) for x in os.environ.get('AUTH_GROUPS', "0").split()]
 AUTH_CHANNEL = os.environ.get('AUTH_CHANNEL', None)
-AUTH_USERS = [int(x) for x in os.environ.get('AUTH_USERS', "").split()]
 CUSTOM_FILE_CAPTION = os.environ.get('CUSTOM_FILE_CAPTION', None)
 API_KEY = os.environ.get('API_KEY', None)
-AUTH_GROUPS = [int(x) for x in os.environ.get('AUTH_GROUPS', "").split()]
 DEV_CHANNEL = os.environ.get('DEV_CHANNEL', "https://t.me/ZauteKm")
-
-print("✅ 환경 변수 로딩 성공.")
