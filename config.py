f7f7e87f40ba7d8c34c33e7830f6b80dcd15012e
alfksdl5877-collab <alfksdl5877@gmail.com>
2026-07-16 22:03:50 +0900

    Update config.py

--- config.py
+++ config.py
@@ -13,57 +13,18 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import re
 import os
-from os import environ
 
-id_pattern = re.compile(r'^.\d+$')
+# 필수 환경 변수만 안전하게 불러오기
+API_ID = int(os.environ.get('API_ID', 0))
+API_HASH = os.environ.get('API_HASH', "")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
+DATABASE_URI = os.environ.get('DATABASE_URI', "")
+DATABASE_NAME = os.environ.get('DATABASE_NAME', "Cluster0")
+ADMINS = [int(x) for x in os.environ.get('ADMINS', "0").split()]
 
-# Bot information
-SESSION = environ.get('SESSION', 'auto-filter-bot-v3')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
-# Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DB_URL = os.environ.get("DATABASE_URI", "")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
-
-# Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-DEV_CHANNEL = "https://t.me/ZauteKm"
-
-# MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-
-# Messages
-default_start_msg = """
-**Hi, I'm [auto-filter-bot-v3](https://github.com/zautekm/auto-filter-bot-v3)**
-
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
-"""
-START_MSG = environ.get('START_MSG', default_start_msg)
-
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
-if FILE_CAPTION.strip() == "":
-    CUSTOM_FILE_CAPTION=None
+# 봇 연결 확인용 로그
+if API_ID != 0 and API_HASH and BOT_TOKEN:
+    print("✅ 필수 환경 변수 로딩 성공: 봇을 시작합니다.")
 else:
-    CUSTOM_FILE_CAPTION=FILE_CAPTION
-if OMDB_API_KEY.strip() == "":
-    API_KEY=None
-else:
-    API_KEY=OMDB_API_KEY
+    print("❌ 환경 변수 오류: API_ID, API_HASH, BOT_TOKEN 중 하나가 비어있습니다.")
