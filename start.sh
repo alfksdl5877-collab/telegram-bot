e001c9f843b5fb98109e49b32c3b67f35caf7a58
alfksdl5877-collab <alfksdl5877@gmail.com>
2026-07-16 20:59:56 +0900

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-python3 main.py
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/auto-filter-bot-v3.git /auto-filter-bot-v3
+cd /auto-filter-bot-v3
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
