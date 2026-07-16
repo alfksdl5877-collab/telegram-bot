db135b955d5f5fec6e95778ff77bf0c8260c781e
alfksdl5877-collab <alfksdl5877@gmail.com>
2026-07-16 21:04:38 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
-echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/auto-filter-bot-v3.git /auto-filter-bot-v3
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+#!/bin/bash
+python3 main.py
